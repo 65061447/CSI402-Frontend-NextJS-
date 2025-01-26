e228888a29191384263e281ebae55f3dc41555ca
65061447 <smith.dip@spumail.net>
2025-01-26 16:17:01 +0700

    first page

---
+++ csi402_lab_approuter/src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  return (
+    <body style={{background : "#c8cbcb" }}>
+    <div style={{marginLeft: "50%",marginTop : "5%",background : ""}}>
+      <h1>ระบบตรวจสอบผู้ใช้งาน</h1>
+    </div>
+    </body>
+  );
+}
